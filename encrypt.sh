1bd36d53580bd6a0e50f26cc72bf7d80de037387
Ethan D. Twardy <ethan.twardy@gmail.com>
2022-06-17 16:39:25 -0500

    Add model crate for interfacing between server and frontend

--- encrypt.sh
+++ encrypt.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 ###############################################################################
 # NAME:		    encrypt.sh
 #
@@ -8,18 +8,27 @@
 #
 # CREATED:	    07/27/2021
 #
-# LAST EDITED:	    06/13/2022
+# LAST EDITED:	    06/17/2022
 ###
 
-ENCRYPTED=filter.rs.age
-DECRYPTED=src/filter.rs
+if [[ -z "$1" || -z "$2" ]]; then
+    >&2 printf 'Usage: %s <encrypt|decrypt> <mod>' "$0"
+    exit 1
+fi
+
+ENCRYPTED="$2.rs.age"
+DECRYPTED="src/$2.rs"
 
 case "$1" in
     decrypt)
         age -d -i ~/.ssh/id_rsa $ENCRYPTED > $DECRYPTED
         ;;
-    *)
+    encrypt)
         age -R ~/.ssh/id_rsa.pub $DECRYPTED > $ENCRYPTED
+        ;;
+    *)
+        >&2 printf 'Usage: %s <encrypt|decrypt> <mod>' "$0"
+        RC=1
         ;;
 esac
 
